1d52c2b5c5951c3cc0f3fea870c2d9b8af73331b
Ghyath <ghyath.darwish@gmail.com>
2023-01-15 22:41:57 +0200

    add

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const { vote_whos_online } = require("./mingle/request")
 const { rate_online_users, whos_online, rate_interested_in_me_users } = require("./request")
 const { default: axios } = require("axios");
 var cron = require('node-cron');
-
+const express = require('express')
+const app = express()
 cron.schedule('*/6 * * * *', async () => {
     console.log('running a task every minute');
     axios.get('https://yeetalk.herokuapp.com/').then(() => { }).catch(() => { })
@@ -17,6 +18,11 @@ cron.schedule('*/6 * * * *', async () => {
         console.log(e);
     }
 });
-
+app.get('/', (rq, rs) => {
+    rs.send("refreshed")
+})
+app.listen(process.env.PORT, () => {
+    console.log('Running on port:', process.env.PORT);
+})
 // whos_online(1).then(console.log)
 // rate_interested_in_me_users()
